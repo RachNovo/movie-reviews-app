8a4b288011249c385e6eabb244816848a9c13fc1
RachNovo <r.novoselac@gmail.com>
2020-03-16 17:06:10 -0400

    edited eslint congif file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,13 @@
 module.exports = {
-  "extends": "airbnb-base"
+  "extends": "airbnb-base",
+  "env": {
+    "browser": true,
+    "es6": true
+  },
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    },
+    "ecmaVersion": 2018,
+    "souceType": "module"
 };
